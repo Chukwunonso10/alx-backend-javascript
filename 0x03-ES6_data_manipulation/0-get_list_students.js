47c7785296afb42caeabae0c3d45da0753740c81
Chukwunonso10 <kuzuechinonsojude@gmai.com>
2024-10-08 14:13:13 -0700

    updates fixed

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,3 @@
-/*
-Create a function named getListStudents that returns an array of objects.
-
-Each object should have three attributes: id (Number), firstName (String), and location (String).
-
-The array contains the following students in order:
-
-Guillaume, id: 1, in San Francisco
-James, id: 2, in Columbia
-Serena, id: 5, in San Francisco
-*/
-
 export default function getListStudents() {
     return [
         {firstName: "Guillaume", id: 1, Location: "in San Francisco"},
@@ -17,7 +5,3 @@ export default function getListStudents() {
         {firstName: "Serena", id: 3, Location: "San Francisco"},
     ];
 }
-
-// let students = getListStudents;
-// console.log(students)
-
