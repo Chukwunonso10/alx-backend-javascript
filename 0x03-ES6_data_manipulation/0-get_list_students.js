12e2877f6b4dfe492175a50af59633ebd81be7dc
Chukwunonso10 <kuzuechinonsojude@gmai.com>
2024-10-08 14:15:54 -0700

    fixed

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
     return [
         {firstName: "Guillaume", id: 1, Location: "in San Francisco"},
         {firstName: "James", id: 2, Location: "in Columbia"},
-        {firstName: "Serena", id: 3, Location: "San Francisco"},
+        {firstName: "Serena", id: 5, Location: "San Francisco"},
     ];
 }
